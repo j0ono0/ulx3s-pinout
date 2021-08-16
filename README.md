68470479cf4a5904091e63be7849eed923d08111
j0ono0 <john@johnnewall.com>
2021-08-16 11:36:39 +0930

    double layout labels test

--- README.md
+++ README.md
@@ -2,20 +2,9 @@
 
 WARNING - under development, incomplete
 
-![pinout_ulx3s.svg](./pinout_ulx3s.svg)
+![pinout_ulx3s_v2.png](./pinout_ulx3s_v2.png)
 
-## Installation
-
-To create a new [diagram.svg](./diagram.svg) file, [pinout](https://pinout.readthedocs.io/) needs to be installed.
-
-## Files
-
-- [data.py](./data.py) contains the pin definitions
-- [ulx3s.png](./ulx3s.png) is the PNG file of the [ULX3S](https://radiona.org/ulx3s/).
-- [styles.css](./styles.css) contains the style sheet for pin label colors.
-- [pinout-update.sh](./pinout-update.sh) watches above files and regenerates the [pinout_ulx3s.svg](./pinout_ulx3s.svg) file.
-- [pinout_ulx3s.svg](./pinout_ulx3s.svg) the SVG file; deleted and recreated when above files are changed.
-
-## References
-
-- [ulx3s_v20.lpf](https://github.com/emard/ulx3s/blob/master/doc/constraints/ulx3s_v20.lpf) - Constraint file.
+**THIS IS JUST A FORK FOR DEVELOPING & EXPERIMENTING WITH ULX3S PINOUT**
+Various pinout visuals for the ulx3s are likely to appear (and disappear) here. Content here are **_not_** necessarily correct. Please refer to the official docs for reference:
+- https://github.com/ulx3s
+- http://radiona.org/ulx3s/
--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 legend = [
-    ("Pin number", "pinid" ),
-    ("GP single-ended", "gpsingle" ),
+    ("Pin number", "pinid"),
+    ("GP single-ended", "gpsingle"),
     ("GN single-ended", "gnsingle"),
-    ("FPGA site","site"),
+    ("FPGA site", "site"),
     ("Analog", "analog"),
     ("Communication", "comms"),
     ("Ground", "gnd"),
@@ -13,52 +13,143 @@ legend = [
 ]
 
 # Pinlabels
-gnd  = ("GND", "gnd")
-pwr  = ("+2.5/3.3V", "pwr")
+gnd = ("GND", "gnd")
+pwr = ("+2.5/3.3V", "pwr")
 
+##############################
+# LHS
 lhs_pairs = (
-    [[pwr]] + [[gnd]]
-    + [[(f"GP{i} | GN{i}","gpio")] for i in range(0, 7)]
-    + [[pwr]] + [[gnd]]
-    + [[(f"GP{i} | GN{i}","gpio")] for i in range(7, 14)]
-    + [[gnd]] + [[pwr]]
-    )
+    [[pwr]]
+    + [[gnd]]
+    + [[(f"GP{i} | GN{i}", "gpio")] for i in range(0, 7)]
+    + [[pwr]]
+    + [[gnd]]
+    + [[(f"GP{i} | GN{i}", "gpio")] for i in range(7, 14)]
+    + [[gnd]]
+    + [[pwr]]
+)
 
-lhs_pairs_numbered = [[(f"{i * 2 + 2} | {i * 2 + 1}","pinid", {"body":{"width":55, "height":20}})] + data for i, data in enumerate(lhs_pairs)]
-#lhs_pairs_numbered = [[(f"({i+j}) " + label[0],  label[1])] for i, row in enumerate(lhs_pairs) for j, label in enumerate(row) ]
+lhs_pairs_numbered = [
+    [(f"{i * 2 + 2} | {i * 2 + 1}", "pinid", {"body": {"width": 55, "height": 20}})]
+    + data
+    for i, data in enumerate(lhs_pairs)
+]
+# lhs_pairs_numbered = [[(f"({i+j}) " + label[0],  label[1])] for i, row in enumerate(lhs_pairs) for j, label in enumerate(row) ]
 
+lhs_outer = (
+    [[pwr]]
+    + [[gnd]]
+    + [[(f"GP{i}", "gpsingle")] for i in range(0, 7)]
+    + [[pwr]]
+    + [[gnd]]
+    + [[(f"GP{i}", "gpsingle")] for i in range(7, 14)]
+    + [[gnd]]
+    + [[pwr]]
+)
+lhs_outer_numbered = [
+    [
+        (
+            f"{i * 2 + 2 }",
+            "pinid",
+            {"body": {"width": 30, "height": 20}},
+        )
+    ]
+    + row
+    for i, row in enumerate(lhs_outer)
+]
+
+
+lhs_inner = (
+    [[pwr]]
+    + [[gnd]]
+    + [[(f"GN{i}", "gnsingle")] for i in range(0, 7)]
+    + [[pwr]]
+    + [[gnd]]
+    + [[(f"GN{i}", "gnsingle")] for i in range(7, 14)]
+    + [[gnd]]
+    + [[pwr]]
+)
+lhs_inner_numbered = [
+    [
+        (
+            f"{i * 2 + 1 }",
+            "pinid",
+            {"body": {"width": 30, "height": 20}},
+        )
+    ]
+    + row
+    for i, row in enumerate(lhs_inner)
+]
+
+
+##############################
+# RHS
 
 rhs_pairs = (
-    [[pwr]] + [[gnd]]
-    + [[(f"GN{i} | GP{i}","gpio")] for i in range(14, 21)]
-    +[[pwr]] + [[gnd]]
-    + [[(f"GN{i} | GP{i}","gpio")] for i in range(21, 28)]
-    + [[gnd]] + [[pwr]]
-    )
-rhs_pairs_numbered = [[(f"{i * 2 + 1 + len(lhs_pairs_numbered)*2} | {i * 2 + 2 + len(lhs_pairs_numbered)*2}","pinid", {"body":{"width":55, "height":20}})] + data for i, data in enumerate(rhs_pairs)]
+    [[pwr]]
+    + [[gnd]]
+    + [[(f"GN{i} | GP{i}", "gpio")] for i in range(14, 21)]
+    + [[pwr]]
+    + [[gnd]]
+    + [[(f"GN{i} | GP{i}", "gpio")] for i in range(21, 28)]
+    + [[gnd]]
+    + [[pwr]]
+)
+rhs_pairs_numbered = [
+    [
+        (
+            f"{i * 2 + 1 + len(lhs_pairs_numbered)*2} | {i * 2 + 2 + len(lhs_pairs_numbered)*2}",
+            "pinid",
+            {"body": {"width": 55, "height": 20}},
+        )
+    ]
+    + data
+    for i, data in enumerate(rhs_pairs)
+]
 
 rhs_outer = (
-    [[pwr]] + [[gnd]]
-    + [[(f"GP{i}","gpsingle")] for i in range(14, 21)]
-    +[[pwr]] + [[gnd]]
-    + [[(f"GP{i}","gpsingle")] for i in range(21, 28)]
-    + [[gnd]] + [[pwr]]
-    )
-rhs_outer_numbered = [[(f"{i * 2 + 2 + len(lhs_pairs_numbered)*2}","pinid", {"body":{"width":30, "height":20}})] + row for i, row in enumerate(rhs_outer)]
+    [[pwr]]
+    + [[gnd]]
+    + [[(f"GP{i}", "gpsingle")] for i in range(14, 21)]
+    + [[pwr]]
+    + [[gnd]]
+    + [[(f"GP{i}", "gpsingle")] for i in range(21, 28)]
+    + [[gnd]]
+    + [[pwr]]
+)
+rhs_outer_numbered = [
+    [
+        (
+            f"{i * 2 + 2 + len(lhs_pairs_numbered)*2}",
+            "pinid",
+            {"body": {"width": 30, "height": 20}},
+        )
+    ]
+    + row
+    for i, row in enumerate(rhs_outer)
+]
 
 rhs_inner = (
-    [[pwr]] + [[gnd]]
-    + [[(f"GN{i}","gnsingle")] for i in range(14, 21)]
-    +[[pwr]] + [[gnd]]
-    + [[(f"GN{i}","gnsingle")] for i in range(21, 28)]
-    + [[gnd]] + [[pwr]]
-    )
-rhs_inner_numbered = [[(f"{i * 2 + 1 + len(lhs_pairs_numbered)*2}","pinid", {"body":{"width":30, "height":20}})] + row for i, row in enumerate(rhs_inner)]
-
-
-
-
-
+    [[pwr]]
+    + [[gnd]]
+    + [[(f"GN{i}", "gnsingle")] for i in range(14, 21)]
+    + [[pwr]]
+    + [[gnd]]
+    + [[(f"GN{i}", "gnsingle")] for i in range(21, 28)]
+    + [[gnd]]
+    + [[pwr]]
+)
+rhs_inner_numbered = [
+    [
+        (
+            f"{i * 2 + 1 + len(lhs_pairs_numbered)*2}",
+            "pinid",
+            {"body": {"width": 30, "height": 20}},
+        )
+    ]
+    + row
+    for i, row in enumerate(rhs_inner)
+]
 
 
 # Text
